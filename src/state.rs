e0aeaec5ef3c2ae20709eed3625404eb82f290b3
EnderDev <kieran@dothq.org>
2023-03-21 20:36:45 +0000

    Rewrite in Tauri

--- src/state.rs
+++
@@ -1,8 +0,0 @@
-use sqlite::sqlite;
-use std::path::Path;
-
-pub fn create_database() {
-    let project_dirs = ProjectDirs::from("dev", "kierand", "students_tourney");
-
-    let connection = sqlite::open(Path::new("")).unwrap();
-}
