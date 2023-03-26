f6a0cfff4cbc8902df5906a831df784bc46c62fa
EnderDev <kieran@dothq.org>
2023-03-26 20:47:36 +0100

    💬 Add rest argument to admin pages

--- src/screens/admin/events.tsx
+++ src/screens/admin/events.tsx
@@ -1,6 +1,8 @@
-export const AdminEvents = () => {
+export const AdminEvents = (
+	rest: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+) => {
 	return (
-		<div className="center-container admin-container">
+		<div {...rest} className="center-container admin-container">
 			<div className={"admin-main center-container"}>
 				<h1>Events</h1>
 				<p>Internal tournament settings</p>
