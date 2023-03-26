b97326898a676889b3160fd78629db7d79174eee
EnderDev <kieran@dothq.org>
2023-03-26 21:19:47 +0100

    ✨ Add table component to events admin page

--- src/screens/admin/events.tsx
+++ src/screens/admin/events.tsx
@@ -5,7 +5,94 @@ export const AdminEvents = (
 		<div {...rest} className="center-container admin-container">
 			<div className={"admin-main center-container"}>
 				<h1>Events</h1>
-				<p>Internal tournament settings</p>
+				<p>Create, modify or delete events</p>
+			</div>
+
+			<div className={"admin-main center-container"}>
+				<h3>All Events</h3>
+
+				<table className={"table"}>
+					<thead>
+						<td>ID</td>
+						<td>Name</td>
+						<td>Type</td>
+						<td>Points Algorithm</td>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Running</td>
+							<td>Individual</td>
+							<td>x=10</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
 	);
